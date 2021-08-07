114b79871638fb7f73d97c06e85e301a5c9bfec3
rajiv1506 <rajiv.pratap26@gmail.com>
2021-08-07 21:45:50 +0530

    Creating a s3 backend

--- main.tf
+++ main.tf
@@ -50,6 +50,7 @@ resource "aws_security_group_rule" "sshtomachine_rule" {
   type = "ingress"
 }
 
+
 resource "aws_security_group_rule" "mediwikiport" {
   cidr_blocks = [ "0.0.0.0/0" ]
   from_port = 80
